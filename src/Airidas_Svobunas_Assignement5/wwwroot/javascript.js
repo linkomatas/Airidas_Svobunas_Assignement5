9bb7ed20eefd4f4d02f0a4ebb7c1d6e42f98e3e4
Student <Student@MB310-83>
2016-09-16 13:43:28 +0300

    I have just added css file.

---
+++ src/Airidas_Svobunas_Assignement5/wwwroot/javascript.js
@@ -0,0 +1,16 @@
+
+    function ChangeColor(id) {
+        document.getElementById(id).style.backgroundColor = "green";
+    }
+function LeaveColor(id) {
+    document.getElementById(id).style.backgroundColor = "grey";
+}
+
+//jQuery
+//          var main = $("content");
+//          main.on("mouseenter", function() {
+//             $("#content").css("background-Color", "yellow")
+//          }};
+//          main.on("mouseleave", function() {
+//               $("#content").css("background-Color", "")
+//           }};
